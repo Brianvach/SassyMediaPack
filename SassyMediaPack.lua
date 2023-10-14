20e64c18777211809f2ada00f42d583e9ba63a43
Brian Vach <brian@vach.me>
2023-10-13 21:01:17 -0400

    Updated version for release

--- SassyMediaPack.lua
+++ SassyMediaPack.lua
@@ -2,7 +2,7 @@ local media = LibStub("LibSharedMedia-3.0")
 
 do
     local SOUND = media.MediaType and media.MediaType.SOUND or "sound"
-    
+
     media:Register(SOUND, "Sassy: Anime", [[Interface/Addons/SassyMediaPack/Sounds/Anime.ogg]])
     media:Register(SOUND, "Sassy: Triple", [[Interface/Addons/SassyMediaPack/Sounds/Triple.ogg]])
 end
